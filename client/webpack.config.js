ce62eac5f88ab18625a6aa1efce32ecbf1fa28f8
paynejc <paynejc84@gmail.com>
2022-05-14 20:24:52 -0400

    forgot to installdb

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,7 +17,7 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'J.A.T.E'
+        title: 'mighty-ocean'
       }),
       new InjectManifest({
         swSrc: './src-sw.js',
@@ -26,8 +26,8 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Just Another Text Editor',
-        short_name: 'J.A.T.E',
+        name: 'mighty-ocean',
+        short_name: 'mighty ocean',
         description: 'Takes notes with JavaScript syntax highlighting!',
         background_color: '#225ca3',
         theme_color: '#225ca3',
